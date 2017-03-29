a754936d947697d132237389d46108cf6692d50e
Thibault Blanc-Beyne <thibault.blancbeyne@gmail.com>
2017-03-29 17:52:54 +0200

    The text generator is now using a degre 3 Markov chain

--- main.cpp
+++ main.cpp
@@ -6,8 +6,7 @@
 
 int main(int argc, char *argv[])
 {
-
-    std::array<std::array<std::array<float, 27>, 27>, 27> probabilities{0};
+    std::array<std::array<std::array<std::array<float, 27>, 27>, 27>, 27> probabilities{0};
     std::ifstream file{"./rechercheDuTempsPerdu.txt"};
     std::vector<char> text;
 
@@ -15,29 +14,33 @@ int main(int argc, char *argv[])
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> letter(1, 10000);
 
-    std::size_t count = 2;
+    std::size_t count = 3;
     char c = file.get();
+    auto prevprevprevindex = c == ' ' ? 26 : static_cast<int>(c) - 97;
+    c = file.get();
     auto prevprevindex = c == ' ' ? 26 : static_cast<int>(c) - 97;
     c = file.get();
     auto previndex = c == ' ' ? 26 : static_cast<int>(c) - 97;
 
+
     while (file.get(c)) {
         count++;
         auto index = c == ' ' ? 26 : static_cast<int>(c) - 97;
-        probabilities[prevprevindex][previndex][index]++;
+        probabilities[prevprevprevindex][prevprevindex][previndex][index]++;
+        prevprevprevindex = prevprevindex;
         prevprevindex = previndex;
         previndex = index;
     }
 
-    for (std::size_t i = 0; i < probabilities.size(); i++)
-        for (std::size_t j = 0; j < probabilities.size(); j++)
-            for (std::size_t k = 0; k < probabilities.size(); k++)
-                probabilities[i][j][k] /= count;
+    for (std::size_t i = 0; i < probabilities.size(); ++i)
+        for (std::size_t j = 0; j < probabilities.size(); ++j)
+            for (std::size_t k = 0; k < probabilities.size(); ++k)
+                for (std::size_t l = 0; l < probabilities.size(); ++l)
+                    probabilities[i][j][k][l] /= count;
 
-    //std::cout << probabilities[0][1][2] << std::endl;
+    int prev,prevprev,prevprevprev;
 
     // First char
-    int prev,prevprev;
     float currLetter = letter(gen)/10000.0;
     std::array<float,27> cumuledProbabilities{0};
     for (std::size_t i = 0; i < probabilities.size(); i++)
@@ -47,7 +50,8 @@ int main(int argc, char *argv[])
         {
             for (std::size_t k = 0; k < probabilities.size(); k++)
             {
-                proba += probabilities[i][j][k];
+                for (std::size_t l = 0; l < probabilities.size(); l++)
+                    proba += probabilities[i][j][k][l];
             }
         }
         cumuledProbabilities[i] = proba;
@@ -69,12 +73,41 @@ int main(int argc, char *argv[])
     // Second char
     currLetter = letter(gen)/10000.0;
     cumuledProbabilities = {0};
+    for (std::size_t i = 0; i < probabilities.size(); i++)
+    {
+        float proba = 0;
+        for (std::size_t j = 0; j < probabilities.size(); j++)
+        {
+            for (std::size_t k = 0; k < probabilities.size(); k++)
+            {
+                proba += probabilities[prev][i][j][k];
+            }
+        }
+        cumuledProbabilities[i] = proba;
+    }
+    for (std::size_t i = 1; i < cumuledProbabilities.size(); i++)
+        cumuledProbabilities[i] += cumuledProbabilities[i-1];
+    for (std::size_t i = 0; i < cumuledProbabilities.size(); i++)
+        cumuledProbabilities[i] /= cumuledProbabilities.at(cumuledProbabilities.size()-1);
+    for (std::size_t i = 0; i < cumuledProbabilities.size(); i++)
+    {
+        if (cumuledProbabilities[i] > currLetter)
+        {
+            text.push_back(i);
+            prevprev = i;
+            break;
+        }
+    }
+
+    // Third char
+    currLetter = letter(gen)/10000.0;
+    cumuledProbabilities = {0};
     for (std::size_t j = 0; j < probabilities.size(); j++)
     {
         float proba = 0;
         for (std::size_t k = 0; k < probabilities.size(); k++)
         {
-            proba += probabilities[prev][j][k];
+            proba += probabilities[prev][prevprev][j][k];
         }
         cumuledProbabilities[j] = proba;
     }
@@ -87,8 +120,7 @@ int main(int argc, char *argv[])
         if (cumuledProbabilities[i] > currLetter)
         {
             text.push_back(i);
-            prevprev = prev;
-            prev = i;
+            prevprevprev = i;
             break;
         }
     }
@@ -96,17 +128,18 @@ int main(int argc, char *argv[])
     // Next chars
     while(text.size() < 10000)
     {
-        int prevprevprev = {0};
+        int prevprevprevprev = 0;
         std::array<float,27> cumuledProbabilities{0};
         currLetter = letter(gen)/10000.0;
-        cumuledProbabilities[0] = probabilities[prevprev][prev][0];
+        cumuledProbabilities[0] = probabilities[prevprevprev][prevprev][prev][0];
         for (std::size_t i = 1; i < probabilities.size(); i++)
-            cumuledProbabilities[i] = cumuledProbabilities[i-1] + probabilities[prevprev][prev][i];
+            cumuledProbabilities[i] = cumuledProbabilities[i-1] + probabilities[prevprevprev][prevprev][prev][i];
         if (cumuledProbabilities.at(cumuledProbabilities.size()-1) == 0)
         {
             text.pop_back();
             prev = prevprev;
             prevprev = prevprevprev;
+            prevprevprev = prevprevprevprev;
             continue;
         }
         for (std::size_t i = 0; i < probabilities.size(); i++)
@@ -116,6 +149,7 @@ int main(int argc, char *argv[])
             if (cumuledProbabilities[i] >= currLetter && (i != 26 || prev != 26))
             {
                 text.push_back(i);
+                prevprevprevprev = prevprevprev;
                 prevprevprev = prevprev;
                 prevprev = prev;
                 prev = i;
