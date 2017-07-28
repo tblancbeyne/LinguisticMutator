e6af16019d20fbdad5179bb307b286a23bb11919
Thibault Blanc-Beyne <thibault.blancbeyne@gmail.com>
2017-07-28 10:55:44 +0200

    May take the number of character in the generated text as 3rd arg

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[])
 {
     mtt::Mutator mut;
-    if (argc == 2)
+    if (argc >= 2)
     {
         mut.addText(argv[1]);
     }
@@ -14,7 +14,17 @@ int main(int argc, char *argv[])
     {
         mut.addText("./rechercheDuTempsPerdu.txt");
     }
-    std::vector<char> text = mut.createText(10000);
+
+    std::vector<char> text;
+
+    if (argc == 3)
+    {
+        text = mut.createText(atoi(argv[2]));
+    }
+    else
+    {
+        text = mut.createText(10000);
+    }
 
     std::cout << text << std::endl;
 
